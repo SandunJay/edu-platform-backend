fe4378861c06656341830a1cb12f485514235cec
Hansanie <hansanieneththasinghe@gmail.com>
2024-05-03 07:15:33 +0530

    payment microservice completed

---
+++ src/main/java/com/example/malpay/MalPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.malpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalPayApplication.class, args);
+	}
+
+}
