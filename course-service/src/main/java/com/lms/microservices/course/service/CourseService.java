7b1da146c12457ca7ba5ba02817740f7690998cf
ChathuniNupearachchi <105709580+ChathuniNupearachchi@users.noreply.github.com>
2024-05-14 17:09:21 +0530

    Update CourseService.java

--- course-service/src/main/java/com/lms/microservices/course/service/CourseService.java
+++ course-service/src/main/java/com/lms/microservices/course/service/CourseService.java
@@ -19,35 +19,38 @@ import java.util.List;
 @Slf4j
 
 public class CourseService {
-
+ 
     private final CourseRepository courseRepository;
 
 
     public CourseResponse createCourse(CourseRequest courseRequest) {
         Course course = Course.builder()
-                .courseId(generateUniqueId())
+                .courseId(generateUniqueId(courseRequest.category()))
                 .name(courseRequest.name())
                 .author(courseRequest.author())
                 .description(courseRequest.description())
+                .learningoutcome(courseRequest.learningoutcome())
+                .category(courseRequest.category())
                 .price(courseRequest.price())
+                .imageurl(courseRequest.imageurl())
                 .build();
         courseRepository.save(course);
         log.info("Course created successfully");
-        return new CourseResponse(course.getId(), course.getCourseId(),course.getName(),course.getAuthor(), course.getDescription(), course.getPrice());
+        return new CourseResponse(course.getId(), course.getCourseId(),course.getName(),course.getAuthor(), course.getDescription(),course.getLearningoutcome(),course.getCategory(),course.getPrice(),course.getImageurl());
     }
 
     public List<CourseResponse> getAllCourses() {
         return courseRepository.findAll()
                 .stream()
-                .map(course -> new CourseResponse(course.getId(),course.getCourseId(), course.getName(),course.getAuthor(), course.getDescription(), course.getPrice()))
+                .map(course -> new CourseResponse(course.getId(),course.getCourseId(), course.getName(),course.getAuthor(), course.getDescription(),course.getLearningoutcome(),course.getCategory(), course.getPrice(),course.getImageurl()))
                 .toList();
     }
 
     public CourseResponse getCourseById(String id) {
         Course course = courseRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Course not found with id " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Course not found with id " + id));
         log.info("Course found successfully");
-        return new CourseResponse(course.getId(),course.getCourseId(), course.getName(),course.getAuthor(), course.getDescription(), course.getPrice());
+        return new CourseResponse(course.getId(),course.getCourseId(), course.getName(),course.getAuthor(), course.getDescription(),course.getLearningoutcome(),course.getCategory(), course.getPrice(),course.getImageurl());
     }
 
     public CourseResponse getCourseByCourseId(String courseId) {
@@ -61,39 +64,55 @@ public class CourseService {
             }
 
             log.info("Course found successfully with courseId " + courseId);
-            return new CourseResponse(course.getId(), course.getCourseId(), course.getName(), course.getAuthor(), course.getDescription(), course.getPrice());
+            return new CourseResponse(course.getId(), course.getCourseId(), course.getName(), course.getAuthor(), course.getDescription(),course.getLearningoutcome(),course.getCategory(), course.getPrice(), course.getImageurl());
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
+ public CourseResponse updateCourse(String courseId, CourseRequest courseRequest) {
+    Course course = courseRepository.findByCourseId(courseId)
+            .orElseThrow(() -> new ResourceNotFoundException("Course not found with id " + courseId));
 
-    public CourseResponse updateCourse(String id, CourseRequest courseRequest) {
-        Course course = courseRepository.findByCourseId(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Course not found with id " + id));
+    if (courseRequest.name() != null) {
         course.setName(courseRequest.name());
-        course.setAuthor(courseRequest.author());
-        course.setDescription(courseRequest.description());
-        course.setPrice(courseRequest.price());
-        courseRepository.save(course);
-        log.info("Course updated successfully");
-        return new CourseResponse(course.getId(), course.getCourseId(),course.getName(),course.getAuthor(), course.getDescription(), course.getPrice());
     }
-
-    public void deleteCourse(String id) {
-        Course course = courseRepository.findByCourseId(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Course not found with id " + id));
+    if (courseRequest.description() != null) {
+        course.setDescription(courseRequest.description());
+    }
+    if (courseRequest.price() != null) {
+        course.setPrice(courseRequest.price());
+    }
+    if (courseRequest.learningoutcome() != null) {
+        course.setLearningoutcome(courseRequest.learningoutcome());
+    }
+    courseRepository.save(course);
+    log.info("Course updated successfully");
+    return new CourseResponse(course.getId(), course.getCourseId(),course.getName(),course.getAuthor(), course.getDescription(),course.getLearningoutcome(),course.getCategory(), course.getPrice(),course.getImageurl());
+}
+    public void deleteCourse(String courseId) {
+        Course course = courseRepository.findByCourseId(courseId)
+                .orElseThrow(() -> new ResourceNotFoundException("Course not found with id " + courseId));
         courseRepository.delete(course);
         log.info("Course deleted successfully");
     }
     public boolean existsCourseById(String id) {
         return courseRepository.existsByCourseId(id);
     }
-    public String generateUniqueId() {
+
+    public String generateUniqueId(String category) {
+    String prefix = switch (category) {
+        case "Information Technology" -> "IT";
+        case "Engineering" -> "E";
+        case "Business Management" -> "BM";
+        case "Arts" -> "A";
+        default -> throw new IllegalArgumentException("Invalid category");
+    };
+
         String id;
-        do {
-            id = "IT" + (int)(Math.random() * 10000);
-        } while(courseRepository.existsById(id));
-        return id;
-    }
+    do {
+        id = prefix + (int)(Math.random() * 10000);
+    } while(courseRepository.existsById(id));
+    return id;
+}
 }
