3ed98ea56797e851087a6894077d39c8a5b2553b
Sandun <sandunjayawardhana2001@gmail.com>
2024-05-16 08:01:23 +0530

    Added new auth service

---
+++ auth-service/src/main/java/com/sanjay/learncentral/config/SecurityConfiguration.java
@@ -0,0 +1,75 @@
+package com.sanjay.learncentral.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static com.sanjay.learncentral.model.Permission.*;
+import static com.sanjay.learncentral.model.Role.ADMIN;
+import static org.springframework.http.HttpMethod.*;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/**").permitAll() // Allow access to authentication endpoints
+                .requestMatchers(POST, "/api/v1/courses").hasRole("ADMIN") // Allow only ADMIN to create course
+                .requestMatchers(PUT, "/api/v1/courses/**").hasAnyRole("ADMIN", "FACULTY","LECTURER") // Allow ADMIN and LECTURER to update course
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/courses/**").hasAnyRole( "ADMIN","FACULTY") // Allow ADMIN and FACULTY to delete course
+                .requestMatchers("/api/v1/courses/**").permitAll() // Allow access to courses endpoints with authentication
+
+                // Room Controller Endpoints
+                .requestMatchers(POST, "/api/v1/rooms").hasRole("ADMIN")
+                .requestMatchers(GET, "/api/v1/rooms/**").authenticated()
+                .requestMatchers(GET, "/api/v1/rooms").hasAnyRole("FACULTY","ADMIN")
+                .requestMatchers(PUT, "/api/v1/rooms/**").hasRole("ADMIN")
+                .requestMatchers(DELETE, "/api/v1/rooms/**").hasRole("ADMIN")
+
+
+                .requestMatchers("/").permitAll() // Allow access to your application's root URL
+                .requestMatchers("/login").permitAll() // Allow access to your application's login page
+                .requestMatchers("/actuator/**").permitAll() // Example: restrict access to actuator endpoints
+                .requestMatchers("/grafana/**").permitAll()
+                .requestMatchers("/v3/api-docs/auth").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/api/v1/auth/logout")
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request, response, authentication) ->
+                        SecurityContextHolder.clearContext())
+
+        ;
+
+        return http.build();
+    }
+}
