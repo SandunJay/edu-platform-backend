b0c924bb0c7c50ad61cfd3df657a288784d5086b
ChathuniNupearachchi <105709580+ChathuniNupearachchi@users.noreply.github.com>
2024-05-16 12:38:01 +0530

    Update ContentResponse.java

--- contents-service/src/main/java/com/lms/microservices/contentsservice/dto/ContentResponse.java
+++ contents-service/src/main/java/com/lms/microservices/contentsservice/dto/ContentResponse.java
@@ -2,5 +2,6 @@ package com.lms.microservices.contentsservice.dto;
 
 import java.util.Date;
 
-public record ContentResponse(String id, String title, String description, String courseId , Date createdDate, Date lastUpdatedDate) {
+
+public record ContentResponse(String id, String title, String description, String courseId ,String videoUrl,String pdfUrl, Date createdDate, Date lastUpdatedDate) {
 }
